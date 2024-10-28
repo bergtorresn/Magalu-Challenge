523bea16d587baa34f775b17565f824512925b9a
Rosemberg Torres Nunes <rosemberg.nunes@catskillet.com>
2024-10-28 12:28:26 -0300

    feature:  creating factories with di

--- Magalu Challenge/Core/DependecyInjector.swift
+++ Magalu Challenge/Core/DependecyInjector.swift
@@ -27,37 +27,37 @@ class DependecyInjector {
         //  ========== Start Popular Repositories
         container.register(PopularListDataSourceProtocol.self) { r in
             return PopularListDataSource(networkService: r.resolve(NetworkServiceProtocol.self)!)
-        }
+        }.inObjectScope(.transient)
         
         container.register(PopularListRepositoryProtocol.self) { r in
             return PopularListRepository(dataSource: r.resolve(PopularListDataSourceProtocol.self)!)
-        }
+        }.inObjectScope(.transient)
         
         container.register(GetPopularRepositoriesUseCaseProtocol.self) { r in
             return GetPopularRepositoriesUseCase(repository: r.resolve(PopularListRepositoryProtocol.self)!)
-        }
+        }.inObjectScope(.transient)
         
         container.register(ListRepositoriesViewModel.self) { r in
             return ListRepositoriesViewModel(usecase: r.resolve(GetPopularRepositoriesUseCaseProtocol.self)!)
-        }
+        }.inObjectScope(.transient)
         //  ========== End Popular Repositories
         
         //  ========== Start Pull Requests
         container.register(PullRequestsDatasourceProtocol.self) { r in
             return PullRequestsDatasource(networkService: r.resolve(NetworkServiceProtocol.self)!)
-        }
+        }.inObjectScope(.transient)
         
         container.register(PullRequestsRepositoryProtocol.self) { r in
             return PullRequestsRepository(dataSource: r.resolve(PullRequestsDatasourceProtocol.self)!)
-        }
+        }.inObjectScope(.transient)
         
         container.register(GetPullRequestsUseCaseProtocol.self) { r in
             return GetPullRequestsUseCase(repository: r.resolve(PullRequestsRepositoryProtocol.self)!)
-        }
+        }.inObjectScope(.transient)
         
         container.register(ListPullRequestsViewModel.self) { r in
             return ListPullRequestsViewModel(usecase: r.resolve(GetPullRequestsUseCaseProtocol.self)!)
-        }
+        }.inObjectScope(.transient)
         //  ========== End Pull Requests
         
     }
